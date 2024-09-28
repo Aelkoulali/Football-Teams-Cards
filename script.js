67d616f8ed8f146f954b9a168d5f8c29014f9143
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-28 08:00:54 +0200

    Declare variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const
+// Declare variables
 const teamName = document.getElementById("team");
 const typeOfSport =  document.getElementById("sport");
 const worldCupYear =  document.getElementById("year");
